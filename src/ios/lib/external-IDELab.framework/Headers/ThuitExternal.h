45acfa4a8d797ff88c726cbdf4aae73fb6d4d70f
mariusz-strawinski <40897193+mariusz-strawinski@users.noreply.github.com>
2018-12-12 15:20:20 +0100

    Create ThuitExternal.h

---
+++ src/ios/lib/external-IDELab.framework/Headers/ThuitExternal.h
@@ -0,0 +1,20 @@
+//
+//  ThuitExternal.h
+//  ThuitExternal
+//
+//  Created by Xavier on 05/10/2018.
+//  Copyright © 2018 selic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThuitExternal.
+FOUNDATION_EXPORT double ThuitExternalVersionNumber;
+
+//! Project version string for ThuitExternal.
+FOUNDATION_EXPORT const unsigned char ThuitExternalVersionString[];
+
+#import <ThuitExternal/TEAppointmentRequest.h>
+#import <ThuitExternal/TEIDELab.h>
+#import <ThuitExternal/TEPatient.h>
+
